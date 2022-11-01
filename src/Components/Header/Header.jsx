c859d30237824c8b9e6146aa869aac215f55ffda
Ashly Margaret <ashlymargaret030@gmail.com>
2022-11-01 11:34:54 +0530

    Hide And Show Menu Done With Use State

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,29 +4,31 @@ import './Header.css'
 import MenuIcon from '../../images/icon-menu.svg'
 import MenuClose from '../../images/icon-menu-close.svg'
 import $ from 'jquery';
-import {useEffect} from 'react'
+import {useEffect,useState} from 'react'
 
 
 
 function Header() {
 
-  useEffect(() => {
-    if($){
-      $('.menuIcon').click(e=>{
-         $('.HideMenu').show()
-      })
+  // useEffect(() => {
+  //   if($){
+  //     $('.menuIcon').click(e=>{
+  //        $('.HideMenu').show()
+  //     })
 
-    }
-  }, [$])
+  //   }
+  // }, [$])
 
-  useEffect(() => {
-    if($){
-      $('.iconMenuClose').click(e=>{
-         $('.HideMenu').hide()
-      })
+  // useEffect(() => {
+  //   if($){
+  //     $('.iconMenuClose').click(e=>{
+  //        $('.HideMenu').hide()
+  //     })
 
-    }
-  }, [$])
+  //   }
+  // }, [$])
+
+  const[show,setShow] = useState(false)
 
   return (
 
@@ -34,7 +36,10 @@ function Header() {
       <div className="logoSec">
            <img src={Logo} alt="logo" />
       </div>
-      <div className="menuIcon">
+      <div className="menuIcon" onClick={()=> {
+        setShow(true)
+        console.log("hiii");
+      }}>
           <img src={MenuIcon} alt="" />
         </div>  
       <div className="navSec"> 
@@ -47,18 +52,26 @@ function Header() {
         </ul>       
       </div>
 
-      <div className="HideMenu">
-        <div className="iconMenuClose">
-          <img src={MenuClose} alt="" />
-        </div>
-        <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">New</a></li>
-            <li><a href="#">Popular</a></li>
-            <li><a href="#">Trending</a></li>
-            <li><a href="#">Catagories</a></li>
-        </ul>
-      </div>
+
+      {
+         
+      show ? <div className="HideMenu">
+          <div className="iconMenuClose" onClick={()=>setShow(false)}>
+            <img src={MenuClose} alt="" />
+          </div>
+          <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">New</a></li>
+          <li><a href="#">Popular</a></li>
+          <li><a href="#">Trending</a></li>
+          <li><a href="#">Catagories</a></li>
+          </ul>
+        </div> : null
+
+
+      }
+
+      
     </div>
 
   )
